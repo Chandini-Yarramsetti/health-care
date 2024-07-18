aff9b3379c210279e009ef12634ba8401ebebf3b
Chandini-Yarramsetti <chandiniyarramsetti1@gmail.com>
2024-07-18 10:44:49 +0530

    Update medicure.tf

--- medicure.tf
+++ medicure.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 # Creating a VPC
 resource "aws_vpc" "project-vpc" {
- cidr_block = "172.16.0.0/16"
+ cidr_block = "192.168. 1.0/16"
 }
 
 # Create an Internet Gateway
@@ -35,7 +35,7 @@ resource "aws_route_table" "project-rt" {
 # Setting up the subnet
 resource "aws_subnet" "project-subnet" {
  vpc_id = aws_vpc.project-vpc.id
- cidr_block = "172.16.0.0/24"
+ cidr_block = "192.168. 1.0/24"
  availability_zone = "us-east-1a"
  tags = {
  Name = "project-subnet1"
@@ -110,12 +110,12 @@ resource "aws_network_interface" "project-ni" {
 resource "aws_eip" "project-eip" {
  vpc = true
  network_interface = aws_network_interface.project-ni.id
- associate_with_private_ip = "172.16.0.0"
+ associate_with_private_ip = "192.168. 1.0"
 }
 
 
 # Creating an ubuntu EC2 instance
-resource "aws_instance" "Jekins-Slave" {
+resource "aws_instance" "JSlave" {
  ami = "ami-04a81a99f5ec58529"
  instance_type = "t2.micro"
  availability_zone = "us-east-1a"
